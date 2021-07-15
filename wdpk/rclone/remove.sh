f0994a59ff4bd1773ce44a2c7d3218332224f1d0
Damian Bucki <damian.bucki@onet.pl>
2021-07-15 19:06:11 +0200

    fix: remove rclone package on uninstall

--- wdpk/rclone/remove.sh
+++ wdpk/rclone/remove.sh
@@ -5,16 +5,16 @@
 path=$1
 
 # create a backup of the config
-APKG_CONFIG="${path}/nzbget.conf"
-APKG_BACKUP_CONFIG="/mnt/HD/HD_a2/.systemfile/nzbget.conf"
+APKG_CONFIG="${path}/rclone.config"
+APKG_BACKUP_CONFIG="/mnt/HD/HD_a2/.systemfile/rclone.config"
 cp ${APKG_CONFIG} ${APKG_BACKUP_CONFIG}
 
 rm -rf $path
 
 # remove bin
-rm -f /usr/bin/nzbget
+rm -f /usr/bin/rclone
 
 # remove lib
 
 # remove web
-rm -rf /var/www/nzbget
+rm -rf /var/www/rclone
